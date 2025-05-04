c0f8c4a94177cd838045b335d27e1c2d26aeffdf
Jakub Szendzielorz <kubasz15@gmail.com>
2025-05-04 18:54:09 +0200

    Update const.py

--- custom_components/pstryk/const.py
+++ custom_components/pstryk/const.py
@@ -4,6 +4,7 @@ API_TIMEOUT = 30
 
 BUY_ENDPOINT = "pricing/?resolution=hour&window_start={start}&window_end={end}"
 SELL_ENDPOINT = "prosumer-pricing/?resolution=hour&window_start={start}&window_end={end}"
+ENERGY_USAGE_ENDPOINT = "meter-data/energy-usage/?for_tz=Europe%2FWarsaw&resolution=day&window_end={end}&window_start={start}"
 
 ATTR_BUY_PRICE = "buy_price"
 ATTR_SELL_PRICE = "sell_price"
